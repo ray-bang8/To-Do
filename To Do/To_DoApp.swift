0a3353b43ed05f22adaf5d979b90d8dbf319a51e
Aray <kanatkyzy98@gmail.com>
2023-09-13 14:32:27 +0600

    Initial Commit

---
+++ To Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To Do
+//
+//  Created by Aray Muratova on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
